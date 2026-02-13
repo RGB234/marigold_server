55620707b731613cf6cfd19b386db2f07fdff972
RGB234 <fehs0611@gmail.com>
2026-02-13 16:15:00 +0900

    deprecated code annotation removal

--- src/main/java/com/sns/marigold/global/util/StringToTsidLongConverter.java
+++ src/main/java/com/sns/marigold/global/util/StringToTsidLongConverter.java
@@ -7,37 +7,8 @@ import org.springframework.stereotype.Component;
 import io.hypersistence.tsid.TSID;
 
 /*
-* 컨트롤러에 '하이픈 없이 넘겨준 UUID 형태 문자열'을 'UUID 객체'로 변환하여 전달
-*/
-
-// @Component
-// public class StringToUuidConverter implements Converter<String, UUID> {
-
-//     @Override
-//     public UUID convert(@NonNull String source) {
-//         if (source == null || source.isEmpty()) {
-//             return null;
-//         }
-
-//         try {
-//             // 1. 하이픈이 없는 32자리 포맷인 경우 -> 하이픈을 삽입하여 파싱
-//             if (!source.contains("-") && source.length() == 32) {
-//                 String formatted = source.replaceFirst(
-//                         "(\\p{XDigit}{8})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}+)",
-//                         "$1-$2-$3-$4-$5");
-//                 return UUID.fromString(formatted);
-//             }
-
-//             // 2. 이미 하이픈이 있거나(36자) 표준 포맷인 경우
-//             return UUID.fromString(source);
-
-//         } catch (IllegalArgumentException e) {
-//             // 잘못된 포맷일 경우 400 Bad Request 등을 유발하기 위해 null 또는 예외 던짐
-//             throw new IllegalArgumentException("Invalid UUID format: " + source);
-//         }
-//     }
-// }
-
+컨트롤러에 전달된 String format의 TSID를 Long 타입으로 변환하여 전달하는 컨버터
+ */
 @Component
 public class StringToTsidLongConverter implements Converter<String, Long> {
 
