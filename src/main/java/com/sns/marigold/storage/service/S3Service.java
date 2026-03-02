f68c2c90684243787a674218fac5f871dbca683b
RGB234 <fehs0611@gmail.com>
2026-03-02 16:30:17 +0900

    refactor: EventListner 통합

--- src/main/java/com/sns/marigold/storage/service/S3Service.java
+++ src/main/java/com/sns/marigold/storage/service/S3Service.java
@@ -10,7 +10,6 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
