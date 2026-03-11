05b8e7d1591306e2bc0d882348bbe78bc09f306c
RGB234 <fehs0611@gmail.com>
2026-03-11 17:10:48 +0900

    [refactor] 매핑 주소 변경

--- src/main/java/com/sns/marigold/user/controller/UserController.java
+++ src/main/java/com/sns/marigold/user/controller/UserController.java
@@ -29,7 +29,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 @Slf4j
 public class UserController {
